ccdb616e9919539c9949f5e5707563b212182c73
Marcin Zepp <nircek-2103@protonmail.com>
2022-10-28 01:04:51 +0200

    Add picking a random app

--- app/src/main/java/io/github/nircek/applicationsieve/RandomPicker.kt
+++ app/src/main/java/io/github/nircek/applicationsieve/RandomPicker.kt
@@ -1,60 +1,55 @@
 package io.github.nircek.applicationsieve
 
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.github.nircek.applicationsieve.databinding.FragmentRandomPickerBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RandomPicker.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RandomPicker : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentRandomPickerBinding? = null
+    private val binding get() = _binding!!
+    private var selectedApp: String? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        // findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        return inflater.inflate(R.layout.fragment_random_picker, container, false)
+    ): View {
+        _binding = FragmentRandomPickerBinding.inflate(inflater, container, false)
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RandomPicker.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RandomPicker().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.randomizeButton.setOnClickListener {
+            val packages = requireActivity().packageManager
+                .getInstalledApplications(PackageManager.GET_META_DATA)
+                .filter{ it.flags and ApplicationInfo.FLAG_SYSTEM == 0 }
+            val packageInfo = packages.random()
+            binding.text.text = "${packages.size} packages. Random: ${packageInfo.packageName} ${packageInfo.sourceDir} ${activity?.packageManager?.getLaunchIntentForPackage(packageInfo.packageName)}"
+            binding.image.setImageDrawable(packageInfo.loadIcon(requireActivity().packageManager))
+            selectedApp = packageInfo.packageName
+        }
+        binding.image.setOnClickListener {
+            val launchIntent =
+                if (selectedApp != null) requireActivity().packageManager.getLaunchIntentForPackage(
+                    selectedApp!!
+                ) else null
+            if (launchIntent != null) startActivity(launchIntent)
+        }
     }
-}
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    }
