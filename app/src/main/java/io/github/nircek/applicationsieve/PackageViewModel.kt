806547f6a553059f23c07ff70df74753b6dd8b3e
Marcin Zepp <nircek-2103@protonmail.com>
2023-01-30 18:16:15 +0100

    Use data binding

--- app/src/main/java/io/github/nircek/applicationsieve/PackageViewModel.kt
+++ app/src/main/java/io/github/nircek/applicationsieve/PackageViewModel.kt
@@ -1,9 +1,15 @@
 package io.github.nircek.applicationsieve
 
+import android.app.Application
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
+import android.widget.Toast
 import androidx.lifecycle.*
 import kotlinx.coroutines.launch
 
-class PackageViewModel(private val repository: PackageRepository) : ViewModel() {
+class PackageViewModel(private val repository: PackageRepository, application: Application) :
+    AndroidViewModel(application) {
 
     // Using LiveData and caching what allWords returns has several benefits:
     // - We can put an observer on the data (instead of polling for changes) and only update the
@@ -11,20 +17,68 @@ class PackageViewModel(private val repository: PackageRepository) : ViewModel() 
     // - Repository is completely separated from the UI through the ViewModel.
     val allPkgs: LiveData<List<Package>> = repository.allPkgs.asLiveData()
 
+    var description = MutableLiveData<String>(app.getString(R.string.app_name))
+    var selectedApp = MutableLiveData<String?>(null)
+    var selectedAppIcon = MutableLiveData<Drawable?>(null)
+    var selectedAppIconStream = MutableLiveData<ByteArray?>(null)
+    var selectedAppRating = MutableLiveData(0.0f)
+    private val app get() = getApplication<Application>()
+    private val pm get() = app.packageManager
+
     /**
      * Launching a new coroutine to insert the data in a non-blocking way
      */
     fun insert(pkg: Package) = viewModelScope.launch {
         repository.insert(pkg)
     }
+
+    fun randomize() {
+        val packages =
+            pm.getInstalledApplications(PackageManager.GET_META_DATA)
+                .filter { it.flags and ApplicationInfo.FLAG_SYSTEM == 0 }
+        val packageInfo = packages.random()
+        description.value =
+            "${packages.size} packages. Random: ${packageInfo.packageName} ${packageInfo.sourceDir} ${
+                pm.getLaunchIntentForPackage(packageInfo.packageName)
+            }"
+        selectedApp.value = packageInfo.packageName
+        selectedAppIcon.value = packageInfo.loadIcon(pm)
+        selectedAppIconStream.value = PackageRepository.drawableToStream(selectedAppIcon.value!!)
+    }
+
+    fun startApp() {
+        val launchIntent =
+            if (selectedApp.value == null) pm.getLaunchIntentForPackage(
+                selectedApp.value!!
+            ) else null
+        if (launchIntent != null) app.startActivity(launchIntent)
+    }
+
+    fun add() {
+        if (selectedApp.value == null) return
+        Toast.makeText(app.applicationContext, "${selectedAppRating.value}/7", Toast.LENGTH_SHORT)
+            .show()
+        insert(
+            Package(
+                selectedApp.value!!,
+                selectedAppIconStream.value!!,
+                selectedAppRating.value!! // why LiveData can be null? maybe it should be StateFlow -- see https://stackoverflow.com/a/64521097/6732111
+            )
+        )
+
+    }
+
 }
 
-class PackageViewModelFactory(private val repository: PackageRepository) :
+class PackageViewModelFactory(
+    private val repository: PackageRepository,
+    private val app: Application
+) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(PackageViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return PackageViewModel(repository) as T
+            return PackageViewModel(repository, app) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
--- app/src/main/java/io/github/nircek/applicationsieve/RandomPicker.kt
+++ app/src/main/java/io/github/nircek/applicationsieve/RandomPicker.kt
@@ -1,13 +1,9 @@
 package io.github.nircek.applicationsieve
 
-import android.content.pm.ApplicationInfo
-import android.content.pm.PackageManager
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,14 +12,11 @@ import io.github.nircek.applicationsieve.databinding.FragmentRandomPickerBinding
 
 class RandomPicker : Fragment() {
 
-    private var _binding: FragmentRandomPickerBinding? = null
-    private val binding get() = _binding!!
-    private var selectedApp: String? = null
-    private var selectedAppIcon: Drawable? = null
-    private var selectedAppIconStream: ByteArray? = null
+    private lateinit var binding: FragmentRandomPickerBinding
 
     private val packageViewModel: PackageViewModel by activityViewModels {
-        PackageViewModelFactory((requireActivity().application as App).repository)
+        val app = requireActivity().application as App
+        PackageViewModelFactory(app.repository, app)
     }
 
 
@@ -31,52 +24,22 @@ class RandomPicker : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentRandomPickerBinding.inflate(inflater, container, false)
-
-        return binding.root
-
+        FragmentRandomPickerBinding.inflate(inflater, container, false).let {
+            binding = it
+            it.viewmodel = packageViewModel
+            it.lifecycleOwner = this
+            return it.root
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.randomizeButton.setOnClickListener {
-            val packages = requireActivity().packageManager
-                .getInstalledApplications(PackageManager.GET_META_DATA)
-                .filter { it.flags and ApplicationInfo.FLAG_SYSTEM == 0 }
-            val packageInfo = packages.random()
-            binding.text.text =
-                "${packages.size} packages. Random: ${packageInfo.packageName} ${packageInfo.sourceDir} ${
-                    activity?.packageManager?.getLaunchIntentForPackage(packageInfo.packageName)
-                }"
-            selectedApp = packageInfo.packageName
-            selectedAppIcon = packageInfo.loadIcon(requireActivity().packageManager)
-            selectedAppIconStream = PackageRepository.drawableToStream(selectedAppIcon!!)
-            binding.image.setImageDrawable(selectedAppIcon)
-        }
-        binding.image.setOnClickListener {
-            val launchIntent =
-                if (selectedApp != null) requireActivity().packageManager.getLaunchIntentForPackage(
-                    selectedApp!!
-                ) else null
-            if (launchIntent != null) startActivity(launchIntent)
-        }
-        binding.add.setOnClickListener {
-            if (selectedApp == null) return@setOnClickListener
-            Toast.makeText(context, "${binding.rating.rating}/7", Toast.LENGTH_SHORT).show()
-            packageViewModel.insert(
-                Package(
-                    selectedApp!!,
-                    selectedAppIconStream!!,
-                    binding.rating.rating
-                )
-            )
-
-        }
-
         val adapter = PackageListAdapter()
-        binding.recyclerView.adapter = adapter
-        binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
+        binding.recyclerView.let {
+            it.adapter = adapter
+            it.layoutManager = LinearLayoutManager(this.context)
+        }
 
         packageViewModel.allPkgs.observe(viewLifecycleOwner) { pks ->
             // Update the cached copy of the pks in the adapter.
@@ -84,10 +47,4 @@ class RandomPicker : Fragment() {
         }
 
     }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
 }
